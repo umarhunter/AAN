ac027f17ee13af64ec56adc05c5958c63da449de
umarhunter <markufaruque@gmail.com>
2024-10-15 16:08:57 -0400

    Add API_KEY

--- news/views.py
+++ news/views.py
@@ -1,7 +1,33 @@
+import requests
+import os
+
 from django.shortcuts import render
+from decouple import config
 from .models import Article
 
+
+def fetch_news():
+    API_KEY = config('API_KEY_1')
+    URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=' + API_KEY
+    response = requests.get(URL)
+    if response.status_code == 200:
+        data = response.json()
+        articles = data.get('articles', [])
+        for item in articles:
+            title = item['title']
+            content = item['description'] or 'No description available'  # Handle missing content
+            published_date = item['publishedAt'][:10]  # Extract date in YYYY-MM-DD format
+
+            # Create and save the article in the database
+            Article.objects.get_or_create(
+                title=title,
+                content=content,
+                published_date=published_date
+            )
+
+
 def home(request):
+    fetch_news()
     articles = Article.objects.order_by('-published_date')
     return render(request, 'news/home.html', {'articles': articles})
 
